5eabc493b5ce05c32926de709dfa4f7fca1eee3a
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-07-12 18:26:00 +0100

    Create case_functions.c

---
+++ case_functions.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_character - prints character
+ * @arg: va_list parameter
+ * Description: print character
+ * Return: 1
+ */
+int print_character(va_list arg)
+{
+	int i;
+
+	i = va_arg(arg, int);
+	_putchar(i);
+
+	return (1);
+}
+/**
+ * print_sign - print sign
+ * @arg: va_list parameter
+ * @base: base 10, 8, 16, 2 etc..
+ * Description: print numbers and signed
+ * Return: num of characters
+ */
+int print_sign(va_list arg, int base)
+{
+	int i = 0, cont = 0;
+	char *s;
+
+	i = va_arg(arg, int);
+	if (i < 0)
+	{
+		i = -(i);
+		_putchar('-');
+		cont += 1;
+	}
